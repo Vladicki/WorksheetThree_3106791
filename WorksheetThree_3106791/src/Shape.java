e37a24be8101e0c58bf9472a6361179e1ec06443
Vlad <vlad.yurev.06@gmail.com>
2024-03-13 23:34:50 +0000

    created Shape and initialized setter/getter

---
+++ WorksheetThree_3106791/src/Shape.java
@@ -0,0 +1,19 @@
+public class Shape {
+
+    private String name;
+
+    // Constructor
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+}
