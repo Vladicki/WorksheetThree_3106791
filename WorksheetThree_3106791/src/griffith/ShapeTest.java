5e5f4367fea375743e9954551d35e7b6a6b97d77
Vlad <vlad.yurev.06@gmail.com>
2024-03-14 17:44:31 +0000

    test are complited and all passes green

--- WorksheetThree_3106791/src/griffith/ShapeTest.java
+++ WorksheetThree_3106791/src/griffith/ShapeTest.java
@@ -1,9 +1,11 @@
 package griffith;
+
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class ShapeTest {
+		
 
     @Test
     void testIntegration() {
@@ -18,30 +20,30 @@ public class ShapeTest {
         shapes.add(new Shape.RightAngledTriangle("Triangle", 5.0, 12.0));
 
         // Assertions for Circle
-        assertEquals(38.48451000647496, shapes.get(0).area(), 0.0);
-        assertEquals(21.991148575128552, shapes.get(0).perimeter(), 0.0);
+        assertEquals(38.48451000647496, shapes.get(0).area());
+        assertEquals(21.991148575128552, shapes.get(0).perimeter());
         assertEquals("Shape: Circle, Radius: 3.5", shapes.get(0).toString());
 
-        assertEquals(78.53981633974483, shapes.get(1).area(), 0.0);
-        assertEquals(31.41592653589793, shapes.get(1).perimeter(), 0.0);
+        assertEquals(78.53981633974483, shapes.get(1).area());
+        assertEquals(31.41592653589793, shapes.get(1).perimeter());
         assertEquals("Shape: Circle, Radius: 5.0", shapes.get(1).toString());
 
         // Assertions for Rhombus
-        assertEquals(6.928203230275509, shapes.get(2).area(), 0.0);
-        assertEquals(16.0, shapes.get(2).perimeter(), 0.0);
+        assertEquals(13.856406460551018, shapes.get(2).area());
+        assertEquals(16.0, shapes.get(2).perimeter());
         assertEquals("Shape: Rhombus, Side: 4.0, Angle: 1.0471975511965976", shapes.get(2).toString());
 
-        assertEquals(18.0, shapes.get(3).area(), 0.0);
-        assertEquals(24.0, shapes.get(3).perimeter(), 0.0);
+        assertEquals(25.45584412271571, shapes.get(3).area());
+        assertEquals(24.0, shapes.get(3).perimeter());
         assertEquals("Shape: Rhombus, Side: 6.0, Angle: 0.7853981633974483", shapes.get(3).toString());
 
         // Assertions for RightAngledTriangle
-        assertEquals(6.0, shapes.get(4).area(), 0.0);
-        assertEquals(12.0, shapes.get(4).perimeter(), 0.0);
+        assertEquals(6.0, shapes.get(4).area());
+        assertEquals(12.0, shapes.get(4).perimeter());
         assertEquals("Shape: Triangle, Base: 3.0, Height: 4.0, Hypotenuse: 5.0", shapes.get(4).toString());
 
-        assertEquals(30.0, shapes.get(5).area(), 0.0);
-        assertEquals(30.0 + 5.0 + 12.0, shapes.get(5).perimeter(), 0.0);
+        assertEquals(30.0, shapes.get(5).area());
+        assertEquals(30.0 , shapes.get(5).perimeter());
         assertEquals("Shape: Triangle, Base: 5.0, Height: 12.0, Hypotenuse: 13.0", shapes.get(5).toString());
     }
 }
